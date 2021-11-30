3d6ce44e9339b539e0f42f6edd026c8a3dc6140f
RolandSallaz <machodin@yandex.ru>
2021-12-01 02:26:10 +0300

    adaptive footer 768 and fix

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,13 +4,13 @@ function Portfolio() {
         <div className='portfolio'>
             <h2 className='portfolio__title'>Портфолио</h2>
             <ul className='portfolio-list'>
-                <li>
+                <li className='portfolio-list__item'>
                     <a className='portfolio-list__link link' href="https://rolandsallaz.github.io/how-to-learn/">Статичный сайт <div className='portfolio-list__link-arrow' /></a>
                 </li>
-                <li>
+                <li className='portfolio-list__item'>
                     <a className='portfolio-list__link link' href="https://rolandsallaz.github.io/russian-travel/">Адаптивный сайт<div className='portfolio-list__link-arrow' /></a>
                 </li>
-                <li>
+                <li className='portfolio-list__item'>
                     <a className='portfolio-list__link link' href="https://rolandsallaz.mesto.nomoredomains.work/sign-in">Одностраничное приложение<div className='portfolio-list__link-arrow' /></a>
                 </li>
             </ul>
