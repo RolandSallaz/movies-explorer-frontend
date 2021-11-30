79853304854476f54eec7c82b45db26769d271e8
RolandSallaz <machodin@yandex.ru>
2021-11-30 03:58:46 +0300

    added portfolio and footer blocks

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import './Footer.css';
+function Footer() {
+    return (
+        <footer className='footer'>
+            <h2 className='footer__title'>Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+            <nav className='footer__links'>
+                <ul className="footer__link-list">
+                    <li><a className='footer__link link' href='https://practicum.yandex.ru/'>Яндекс.Практикум</a></li>
+                    <li><a className='footer__link link' href="https://github.com/RolandSallaz">Github</a></li>
+                    <li><a className='footer__link link' href='facebook.com/'>Facebook</a></li>
+                </ul>
+            </nav>
+            <p className="footer__copyright ">&copy; 2021</p>
+        </footer>
+    )
+}
+export default Footer;
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,20 @@
+import './Portfolio.css';
+function Portfolio() {
+    return (
+        <div className='portfolio'>
+            <h2 className='portfolio__title'>Портфолио</h2>
+            <ul className='portfolio-list'>
+                <li>
+                    <a className='portfolio-list__link link' href="https://rolandsallaz.github.io/how-to-learn/">Статичный сайт <div className='portfolio-list__link-arrow' /></a>
+                </li>
+                <li>
+                    <a className='portfolio-list__link link' href="https://rolandsallaz.github.io/russian-travel/">Адаптивный сайт<div className='portfolio-list__link-arrow' /></a>
+                </li>
+                <li>
+                    <a className='portfolio-list__link link' href="https://rolandsallaz.mesto.nomoredomains.work/sign-in">Одностраничное приложение<div className='portfolio-list__link-arrow' /></a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+export default Portfolio;
