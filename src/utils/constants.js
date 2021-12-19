bdb47b07e4bffadfd4005223bbab299ce3bdf1c3
Roland <machodin@yandex.ru>
2021-12-19 17:07:54 +0300

    fix api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
 const apiUrl = 'https://api.diploma.roland.nomoredomains.work';
-const apiUrl = 'http://localhost:3000';
 const filmsUrl = 'https://api.nomoreparties.co';
 const shortFilmDuration = 40;
 const initialMoviesPc = 12;
